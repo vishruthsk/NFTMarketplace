50cc34a0d711ed2da6d1414a22e04ebd72bffc44
vishruth s k <vishruthsk@vishruths-MacBook-Air.local>
2022-07-22 14:34:46 +0530

    added README.md

---
+++ hardhat/contracts/CeloNFT.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract CeloNFT is ERC721{
+
+    constructor() ERC721("CeloNFT","cNFT"){
+        for(uint i=0;i<5;i++){
+            _mint(msg.sender, i);
+        }
+
+        
+    }
+    function tokenURI(uint)public pure override returns(string memory){
+            return "ipfs://QmTy8w65yBXgyfG2ZBg5TrfB2hPjrDQH3RCQFJGkARStJb";
+        }
+}
